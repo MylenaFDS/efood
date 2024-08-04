5ec17965e927f778685a0617cbb80fb3696cf7a7
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-08-04 18:29:09 -0300

    ...

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,4 +1,21 @@
-declare module '*.png';
-declare module '*.svg';
-declare module '*.jpg';
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.jpeg" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.png" {
+    const value: string;
+    export default value;
+  }
+  
+  declare module "*.svg" {
+    const value: string;
+    export default value;
+  }
+  
 
