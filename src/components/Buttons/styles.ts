e5d7defdbe948c41240283d36629c391128c5ac5
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-08-08 22:06:33 -0300

    ...

--- src/components/Buttons/styles.ts
+++ src/components/Buttons/styles.ts
@@ -9,7 +9,7 @@ export const ButtonContainer = styled.button`
   width: 90px;
   height: 30px;
   left: 8px;  /* Ajuste conforme necessário */
-  margin-top: 240px;  /* Ajuste conforme necessário */
+  top: 140px;  /* Ajuste conforme necessário */
   background-color: ${cores.coral};
   color: white;
   border: none;
@@ -26,7 +26,7 @@ position: relative;
 width: 90px;
 height: 30px;
 left: 8px;  /* Ajuste conforme necessário */
-margin-top: 240px;  /* Ajuste conforme necessário */
+top: 140px;  /* Ajuste conforme necessário */
 background-color: ${cores.coral};
 color: white;
 border: none;
