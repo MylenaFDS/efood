dc5ae568a97f7c3b7c8cdbd8ea4f89f703457101
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-09-26 01:01:08 -0300

    criando a const breakpoints

--- src/styles.ts
+++ src/styles.ts
@@ -1,26 +1,33 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
 export const cores = {
-  coral:'#E66767',
+  coral: '#E66767',
   branca: '#FFF8F2',
   creme: '#FFEBD9',
-  coralDark:'#CD5B45'
+  coralDark: '#CD5B45'
+}
+
+export const breakpoints = {
+  desktop: '1024px',
+  tablet: '821px',
+  mobile: '430px',
+  iphone11: '414px'
 }
 
 export const GlobalCss = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: Roboto, sans-serif;
-    list-style: none;
-  }
+*{
+  margin: 0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: Roboto, sans-serif;
+  list-style: none;
+}
+
 
   body {
     background-color: ${cores.branca};
     color: ${cores.branca};
-    padding-top: 40px;
-    object-cover:fit;
+    
     overflow-x: hidden;
   }
 
@@ -31,9 +38,10 @@ export const GlobalCss = createGlobalStyle`
   }
 `
 
-export const HomeContainer = {
-position: 'relative',
-width: '1366px',
-height: '2148px',
-background: '#FFF8F2'
-}
+// Aqui você usa um Styled Component para HomeContainer
+export const HomeContainer = styled.div`
+  position: relative;
+  width: 1366px;
+  height: 2148px;
+  background-color: ${cores.branca};
+`
