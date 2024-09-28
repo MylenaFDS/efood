fa8b356a111244a46e3a483d72036bb0b3bc9dc3
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-09-27 22:22:01 -0300

    ...

--- src/components/RestaurantListing/styles.ts
+++ src/components/RestaurantListing/styles.ts
@@ -1,24 +1,27 @@
 import styled from 'styled-components';
 import { cores } from '../../styles';
+
 export const ListingContainer = styled.div`
   position: absolute;
   width: 1024px;
-height: 1290px;
-top: 40px;
-
-
+  height: 1290px;
+  top: 40px;
+  left: 0;
+  right: 0;
+  margin: auto; /* Centraliza horizontalmente */
 `;
+
 export const Restaurant = styled.div`
   position: absolute;
   width: 472px;
   height: 398px;
   background-color: ${cores.branca};
   border: 1px solid ${cores.coral};
-  
 `;
+
 export const Highlight = styled.span`
   position: absolute;
-  padding:5px;
+  padding: 5px;
   width: 150px;
   height: 26px;
   left: 210px;
@@ -26,30 +29,31 @@ export const Highlight = styled.span`
   font-family: 'Roboto', sans-serif;
   font-weight: 700;
   font-size: 14px;
-  text-align:center;
+  text-align: center;
   background-color: ${cores.coral};
   color: ${cores.creme};
 `;
+
 export const Tag = styled.div`
   position: absolute;
-  padding:5px;
+  padding: 5px;
   width: 75px;
   height: 26px;
   left: 370px;
   top: 16px;
   background-color: ${cores.coral};
   color: ${cores.creme};
-  align-itens:center;
+  align-items: center;
   text-align: center;
   display: inline-block;
- 
   font-family: 'Roboto', sans-serif;
   font-weight: 700;
   font-size: 14px;
 `;
+
 export const RestaurantTitle = styled.h2`
   position: absolute;
-  width: auto; /* Ajuste a largura para se adaptar ao conteúdo */
+  width: auto;
   height: 21px;
   left: 8px;
   top: 225px;
@@ -58,13 +62,13 @@ export const RestaurantTitle = styled.h2`
   font-size: 18px;
   line-height: 21px;
   color: ${cores.coral};
-  white-space: nowrap; /* Impede a quebra de linha */
-  overflow: hidden; /* Opcional: esconde o texto que ultrapassa a largura */
-  text-overflow: ellipsis; /* Opcional: adiciona "..." no final do texto, se necessário */
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
+
 export const RestaurantDescription = styled.p`
-position: absolute;
-  
+  position: absolute;
   width: 456px;
   height: 88px;
   left: 8px;
@@ -73,4 +77,5 @@ position: absolute;
   font-weight: 400;
   font-size: 14px;
   line-height: 22px;
-  color: ${cores.coral}`;
+  color: ${cores.coral};
+`;
