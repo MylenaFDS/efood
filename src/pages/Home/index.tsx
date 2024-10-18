3945953117745772e940c190ec1884ad4e63a52e
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-10-18 18:30:05 -0300

    ...

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import Hero from "../../components/Hero";
 import RestaurantListing from "../../components/RestaurantListing";
 import estrela from '../../assets/images/estrela.jpg'
 import RestaurantModel  from '../../modals/RestaurantModal';
-const restaurants:RestaurantModel[] = [
+const restaurantes:RestaurantModel[] = [
     {
       id: 1,
       name: 'Bella Tavolla Italiana',
